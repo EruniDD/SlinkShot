3d29143e81ceec130ce9d4bdef547f69df94097d
EruniDD <66692106+EruniDD@users.noreply.github.com>
2020-11-28 16:57:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
 import cv2
 import numpy as np
 import pyautogui
+import keyboard  # using module keyboard
 from pynput.keyboard import Listener
 
+print("import ok")
+
 def on_press(key):
-    if str(key) == '\'q\'':
-        Rec()
-    pass
+    global isRec
+    print(key)
+    if str(key) == "\'q\'":
+        isRec = False
+        print(isRec)
 
 def on_release(key):
     pass
-
-def Rec():
-    while True:
-        print("rec")
-        # make a screenshot
-        img = pyautogui.screenshot()
-        # img = pyautogui.screenshot(region=(0, 0, 300, 400))
-        # convert these pixels to a proper numpy array to work with OpenCV
-        frame = np.array(img)
-        # convert colors from BGR to RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # write the frame
-        out.write(frame)
-        # show the frame
-        # cv2.imshow("screenshot", frame)
-        # if the user clicks q, it exits
-        # print(img)
-
-
-print("import ok")
 
 # display screen resolution, get it from your OS settings
 SCREEN_SIZE = (1920, 1080)
 # define the codec
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
-# create the video write object
+# create the video write objectq
 out = cv2.VideoWriter("output.avi", fourcc, 20.0, (SCREEN_SIZE))
 
-while True:
-    with Listener(on_press=on_press) as listener:
-        listener.join()
+listaFrame = []
 
+isRec = True
+
+while isRec == True:
+    if(keyboard.is_pressed('q')):
+        print("ciao")
+        break
+    # make a screenshot
+    img = pyautogui.screenshot()
+    # img = pyautogui.screenshot(region=(0, 0, 300, 400))
+    # convert these pixels to a proper numpy array to work with OpenCV
+    frame = np.array(img)
+    # convert colors from BGR to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # write the frame
+    print(len(listaFrame))
+    if (len(listaFrame) == 600):
+        listaFrame.pop(0)
+        listaFrame.append(frame)
+    else:
+        listaFrame.append(frame)
+
+    # out.write(frame)
+    # show the frame
+    # cv2.imshow("screenshot", frame)
+    # if the user clicks q, it exits
+    # print(img)
+
+for x in listaFrame:
+    out.write(x)
 
 # make sure everything is closed when exited
 cv2.destroyAllWindows()
